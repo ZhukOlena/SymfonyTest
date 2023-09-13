f4797572efbb9980d9a36f5efe8cee8afa475c12
Olena Zhuk <olena1303@gmail.com>
2023-09-13 17:13:28 +0300

    route-params-requirements

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -10,15 +10,18 @@ class HelloController
     private array $messages =[
         "Hello", "Hi", "Bye!"
     ];
-    #[Route('/', name: 'app_index')]
-    public function index(): Response
+    #[Route('/{limit<\d+>?3}', name: 'app_index')]
+    public function index(int $limit): Response
     {
-        return new Response(implode(',', $this->messages));
+        return new Response(
+            implode(',', array_slice($this->messages, 0, $limit))
+        );
     }
 
-    #[Route('/message/{id}', name: 'app_show_one')]
+    #[Route('/message/{id<\d+>}', name: 'app_show_one')]
     public function showOne($id): Response
     {
         return new Response ($this->messages[$id]);
     }
 }
+
