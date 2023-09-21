ae8bd87168c13cfbf9dfd33d49de6ce90b65005f
Olena Zhuk <olena1303@gmail.com>
2023-09-21 11:56:33 +0300

    twig-filters-functions

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -15,7 +15,10 @@ class HelloController extends AbstractController
     public function index(int $limit): Response
     {
         return $this->render('hello/index.html.twig',
-        ['message' => implode(',', array_slice($this->messages, 0, $limit))]
+        [
+            'messages' => $this->messages,
+            'limit' => $limit
+        ]
         );
     }
 
